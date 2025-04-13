f5b67573f1b07ad2fb1d725393e5db1b676adace
naji-alhusami <najihussami@gmail.com>
2025-04-13 23:30:18 +0200

    Finished Home Page

--- Frontend/src/components/Home/Home.tsx
+++ Frontend/src/components/Home/Home.tsx
@@ -1,5 +1,55 @@
-function Home() {
-  return <div className="text-amber-800 font-bold">Home</div>;
-}
+import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+
+import { HomeSlidesData } from "./HomeSlidesData";
+import "./Home.css";
+
+const Home = () => {
+  const [currentSlideIndex, setCurrentSlideIndex] = useState<number>(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlideIndex(
+        (prevIndex) => (prevIndex + 1) % HomeSlidesData.length
+      );
+    }, 6000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div
+      key={currentSlideIndex}
+      className="h-screen  flex items-center justify-center bg-cover bg-top bg-no-repeat"
+      style={{
+        backgroundImage: `url(${HomeSlidesData[currentSlideIndex].background})`,
+        animation: "fadeIn 5s",
+      }}
+    >
+      <div className="flex flex-col mx-5">
+        <motion.div
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 2, duration: 1 }}
+          className="rounded-t-md px-5 pt-5 pb-1 bg-opacity-50 bg-black flex flex-col"
+        >
+          <h1 className="text-2xl md:text-3xl lg:text-4xl text-white text-center">
+            {HomeSlidesData[currentSlideIndex].heading}
+          </h1>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 2, duration: 1 }}
+          className="rounded-b-md px-5 pb-5 pt-1 bg-opacity-50 bg-black flex flex-col"
+        >
+          <p className="text-2xl md:text-3xl lg:text-4xl text-white text-center">
+            {HomeSlidesData[currentSlideIndex].subheading}
+          </p>
+        </motion.div>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
---
+++ Frontend/src/components/Home/HomeSlidesData.ts
@@ -0,0 +1,52 @@
+import bg1 from "../../Images/bg1.jpg";
+import bg2 from "../../Images/bg2.jpg";
+import bg3 from "../../Images/bg3.jpg";
+import bg4 from "../../Images/bg4.jpg";
+import bg5 from "../../Images/bg5.jpg";
+import bg6 from "../../Images/bg6.jpg";
+
+type Slide = {
+  background: string;
+  heading: string;
+  subheading: string;
+  buttonText: string;
+};
+
+export const HomeSlidesData: Slide[] = [
+  {
+    background: bg1,
+    heading: "Start Your Journey Of Discovery In Istanbul,",
+    subheading: "Where Others Have Ventured Before You",
+    buttonText: "ICONIC PLACES",
+  },
+  {
+    background: bg2,
+    heading: "Escape To Istanbul,",
+    subheading: "Your Dream Destination Brought To Life.",
+    buttonText: "ICONIC PLACES",
+  },
+  {
+    background: bg3,
+    heading: "Help Others Uncover New Places In Istanbul,",
+    subheading: "Making Their Exploration More Remarkable",
+    buttonText: "ICONIC PLACES",
+  },
+  {
+    background: bg4,
+    heading: "Unearth The Captivating Sights Of Istanbul,",
+    subheading: "Cherished By All Who Venture There",
+    buttonText: "ICONIC PLACES",
+  },
+  {
+    background: bg5,
+    heading: "Discover Unique Views In Istanbul,",
+    subheading: "Where The Eastern and Western Cultures Awaits",
+    buttonText: "ICONIC PLACES",
+  },
+  {
+    background: bg6,
+    heading: "Remember To Capture The Mesmerizing Moments,",
+    subheading: "And Share Them On Our Website",
+    buttonText: "ICONIC PLACES",
+  },
+];
