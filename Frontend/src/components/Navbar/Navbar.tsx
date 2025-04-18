c17b42b21e17202ab94c4004e3b81d1be4c6823f
naji-alhusami <najihussami@gmail.com>
2025-04-18 22:21:13 +0200

    Added Responsive Navbar in defferent component

--- Frontend/src/components/Navbar/Navbar.tsx
+++ Frontend/src/components/Navbar/Navbar.tsx
@@ -1,9 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { FaHome } from "react-icons/fa";
-import { MdPlace } from "react-icons/md";
-import { GoBookmarkFill } from "react-icons/go";
 
+import ResponsiveNavbar from "./ResponsiveNavbar";
 import Button from "../ui/Button";
 
 function Navbar() {
@@ -78,47 +76,7 @@ function Navbar() {
       </section>
 
       {/* Middle and Small Screens */}
-      {showSideNavbar && (
-        <div
-          className="italic font-bold text-lg bg-white absolute top-12 right-0 w-full sm:absolute sm:top-14 sm:right-13 sm:w-60 sm:rounded-md divide-y divide-gray-300 border border-gray-300 shadow-lg md:hidden"
-          onClick={toggleButtonHandler}
-        >
-          <Link to="/" className="px-12 h-12 flex items-center justify-start">
-            <FaHome />
-            <h1 className="pl-4">HOME</h1>
-          </Link>
-          <Link
-            to="/places"
-            className="px-12 h-12 flex items-center justify-start"
-          >
-            <MdPlace />
-            <h1 className="pl-4">PLACES</h1>
-          </Link>
-          <Link
-            to="/about"
-            className="px-12 h-12 flex items-center justify-start"
-          >
-            <GoBookmarkFill />
-            <h1 className="pl-4">ABOUT</h1>
-          </Link>
-          <div className="h-12 flex items-center justify-center">
-            <Button
-              type="button"
-              className="w-full mx-12 text-purple-900 border border-purple-900 rounded-md cursor-pointer hover:bg-purple-700 hover:text-white text-lg"
-            >
-              Login
-            </Button>
-          </div>
-          <div className="h-10 flex items-center justify-center">
-            <Button
-              type="button"
-              className="w-full mx-12 text-white border border-purple-900 rounded-md cursor-pointer bg-purple-700 hover:bg-purple-900 text-lg"
-            >
-              Signup
-            </Button>
-          </div>
-        </div>
-      )}
+      {showSideNavbar && <ResponsiveNavbar toggleButtonHandler={toggleButtonHandler} />}
     </nav>
   );
 }
