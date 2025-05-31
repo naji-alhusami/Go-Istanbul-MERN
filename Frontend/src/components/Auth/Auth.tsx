c55250b53f892951fa3eb3f08bc8924a3eefa799
naji-alhusami <najihussami@gmail.com>
2025-05-31 11:57:28 +0200

    Finsihed Signup form

--- Frontend/src/components/Auth/Auth.tsx
+++ Frontend/src/components/Auth/Auth.tsx
@@ -1,5 +1,77 @@
+import Button from "../ui/Button";
+import Input from "../ui/Input";
+import SignupImage from "../../Images/SignupImage.png";
+
 const Auth = () => {
-  return <div>Auth</div>;
+  return (
+    <div className="lg:p-10 w-full flex flex-row items-center justify-center">
+      <div className="hidden lg:flex md:items-center md:justify-center lg:w-xl">
+        <img src={SignupImage} alt="SignupImage" className="w-90 h-90" />
+      </div>
+      <div className="w-[28rem] p-10 md:p-2 flex flex-col justify-center items-center lg:w-[32rem]">
+        <div className="text-center mt-2 mb-10">
+          <h1
+            // onClick={() => navigate("/")}
+            className=" poetsen-one-regular text-purple-900 text-4xl"
+          >
+            Go World
+          </h1>
+          <p className="my-4 text-gray-500 font-bold">
+            Join now to upload private albums and showcase the stunning cities
+            you explore. Help others discover the best spots while keeping your
+            personal memories safe.
+          </p>
+        </div>
+        <form className="w-full flex flex-col justify-center items-center">
+          <Input
+            htmlFor="FullName"
+            label="Full Name"
+            placeholder="Your Full Name"
+            type="text"
+          />
+          <Input
+            htmlFor="Email"
+            label="Email"
+            placeholder="Your Email"
+            type="email"
+          />
+          <Input
+            htmlFor="Password"
+            label="Password"
+            placeholder="Your Password"
+            type="password"
+          />
+          <Input
+            htmlFor="ConfirmPassword"
+            label="Confirm Password"
+            placeholder="Your Password Confirmation"
+            type="password"
+          />
+          <div className="w-full mt-4 mb-2">
+            <Button
+              type="submit"
+              className="w-full py-1 text-white border border-purple-900 rounded-md cursor-pointer bg-purple-700 hover:bg-purple-900 text-lg"
+            >
+              Signup
+            </Button>
+          </div>
+          <div className="flex items-center my-4 w-full">
+            <div className="h-px flex-grow bg-gray-300" />
+            <span className="mx-4 text-gray-500">OR</span>
+            <div className="h-px flex-grow bg-gray-300" />
+          </div>
+          <div className="w-full mt-2">
+            <Button
+              type="button"
+              className="w-full py-1 text-purple-900 border border-purple-900 rounded-md cursor-pointer hover:bg-purple-700 hover:text-white text-lg"
+            >
+              Login
+            </Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
--- Frontend/src/components/ui/Input.tsx
+++ Frontend/src/components/ui/Input.tsx
@@ -1,5 +1,54 @@
-const Input = () => {
-  return <div>Input</div>;
+type InputProps = {
+  htmlFor: string;
+  label: string;
+  type: "text" | "email" | "password";
+  placeholder?: string;
+  value?: string;
+  error?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onFocus?: (e: React.FocusEvent<HTMLInputElement>) => void;
+  onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
+};
+
+const Input: React.FC<InputProps> = ({
+  htmlFor,
+  label,
+  type,
+  placeholder,
+  value,
+  error,
+  onChange,
+  onFocus,
+  onBlur,
+}) => {
+  return (
+    <div className="w-full my-2">
+      <label
+        htmlFor={htmlFor}
+        className="block mb-0.5"
+        // style={{ display: "block", marginBottom: "0.25rem" }}
+      >
+        {label}
+      </label>
+      <input
+        // id={htmlFor}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        className={`w-full p-2 rounded border ${
+          error ? "border-red-500" : "border-gray-300"
+        }`}
+      />
+      {error && (
+        <p style={{ color: "red", fontSize: "0.8rem", marginTop: "0.25rem" }}>
+          {error}
+        </p>
+      )}
+    </div>
+  );
 };
 
 export default Input;
